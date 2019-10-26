5605e396a44ff0450a457a3fed0e0378d47a99cc
antonsipin <anton.sipin@gmail.com>
2019-10-26 06:45:46 +0300

    Простое число.[#189047]

---
+++ chapter_001/src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -0,0 +1,17 @@
+package ru.job4j.loop;
+
+public class CheckPrimeNumber {
+    public boolean check(int num) {
+        boolean prime = false;
+        for (int i = 1; i <= num; i++) {
+            if (num % 2 == 0) {
+                if (num % i == 0) {
+                    prime = false;
+                }
+            } else if (num % 1 == 0 && num % i == 0) {
+                prime = true;
+            }
+        }
+        return prime;
+    }
+}
