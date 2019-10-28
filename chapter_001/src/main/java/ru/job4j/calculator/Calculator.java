3ea6d7464a39c4ad5dce0a19151f30edd2e18f39
antonsipin <anton.sipin@gmail.com>
2019-10-28 06:17:41 +0300

    JavaDoc. Code convention[#189042]

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,24 +1,37 @@
 package ru.job4j.calculator;
 
+/**
+ * Класс Calculator. Арифметические операции с переменными first и second. Вывод результатов в консоль.
+ *
+ * @author Anton Sipin (anton.sipin@gmail.com)
+ */
 public class Calculator {
 
     public static void add(double first, double second) {
-        double result =  first + second;
-        System.out.println(first + " + " + second + " = " + result);
-    }
-    public static void div(double first, double second) {
-        double result =  first / second;
-        System.out.println(first + " + " + second + " = " + result);
-    }
-    public static void multiply(double first, double second) {
-        double result =  first * second;
-        System.out.println(first + " + " + second + " = " + result);
-    }
-    public static void subtract(double first, double second) {
-        double result =  first - second;
+        double result = first + second;
         System.out.println(first + " + " + second + " = " + result);
     }
 
+    public static void div(double first, double second) {
+        double result = first / second;
+        System.out.println(first + " + " + second + " = " + result);
+    }
+
+    public static void multiply(double first, double second) {
+        double result = first * second;
+        System.out.println(first + " + " + second + " = " + result);
+    }
+
+    public static void subtract(double first, double second) {
+        double result = first - second;
+        System.out.println(first + " + " + second + " = " + result);
+    }
+
+    /**
+     * Метод Main.
+     *
+     * @param args - аргументы метода.
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
