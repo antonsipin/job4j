57d71a6ec084b0c917c6cbaca19c7843648b5d7e
antonsipin <anton.sipin@gmail.com>
2019-10-29 07:16:50 +0300

    Построить шахматную доску в псевдографике.[#189043]

--- chapter_001/src/main/java/ru/job4j/loop/Slash.java
+++ chapter_001/src/main/java/ru/job4j/loop/Slash.java
@@ -4,9 +4,7 @@ public class Slash {
     public static void draw(int size) {
         for (int row = 0; row < size; row++) {
             for (int column = 0; column < size; column++) {
-                if (row == column) {
-                    System.out.print("0");
-                } else if (((column == size - 1) && (row == 0)) || ((column == 0) && (row == size - 1)) || ((column == size - 2) && (row == size - 4)) || ((column == size - 4) && (row == size - 2))) {
+                if (row == column || ((row + column) % 2 == 0) && ((row + column) == size - 1)) {
                     System.out.print("0");
                 } else {
                     System.out.print(" ");
