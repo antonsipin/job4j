36967ea3f8e3e9be7ea130be85c0768852cb3d11
antonsipin <anton.sipin@gmail.com>
2019-10-29 06:30:49 +0300

    Простые числа.[#189045]

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -3,8 +3,8 @@ package ru.job4j.loop;
 public class PrimeNumber {
     public int calc(int finish) {
         int count = 0;
-        for (int value = 2; value <= finish; value++) {
-            if (((value == 2) || (value % 2 != 0)) && (value % 1 == 0) && (value % value == 0)) {
+        for (int i = 2; i <= finish; i++) {
+            if (i % 2 != 0 || i == 2) {
                 count++;
             }
         }
