7d211c7fb9ecfeb29ae8d37b8ae1db8ee9950acc
antonsipin <anton.sipin@gmail.com>
2019-11-01 06:41:52 +0300

    Классический поиск перебором.[#189051]

--- chapter_001/src/main/java/ru/job4j/array/anton/Square.java
+++ chapter_001/src/main/java/ru/job4j/array/anton/Square.java
@@ -1,4 +1,4 @@
-package ru.job4j.array;
+package ru.job4j.array.anton;
 
 public class Square {
     public int[] calculate(int bound) {
--- chapter_001/src/test/java/ru/job4j/array/SquareTest.java
+++ chapter_001/src/test/java/ru/job4j/array/SquareTest.java
@@ -1,6 +1,8 @@
 package ru.job4j.array;
 
 import org.junit.Test;
+import ru.job4j.array.anton.Square;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
