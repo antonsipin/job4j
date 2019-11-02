ae643d6f357afb9991d9f4c92f8dfcb04c75990b
antonsipin <anton.sipin@gmail.com>
2019-11-02 17:50:30 +0300

    Сортировка выборкой.[#189063]

--- chapter_001/src/test/java/ru/job4j/array/anton/FindLoopTest.java
+++ chapter_001/src/test/java/ru/job4j/array/anton/FindLoopTest.java
@@ -1,15 +1,17 @@
 package ru.job4j.array.anton;
 
 import org.junit.Test;
+
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
+import static ru.job4j.array.anton.FindLoop.sort;
 
 public class FindLoopTest {
 
     @Test
     public void whenArrayHas5Then0() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 3};
+        int[] input = new int[]{5, 10, 3};
         int value = 5;
         int result = find.indexOf(input, value);
         int expect = 0;
@@ -19,7 +21,7 @@ public class FindLoopTest {
     @Test
     public void whenArrayHasNot7ThenMinus1() {
         FindLoop find = new FindLoop();
-        int[] input = new int[] {5, 10, 3};
+        int[] input = new int[]{5, 10, 3};
         int value = 7;
         int result = find.indexOf(input, value);
         int expect = -1;
@@ -28,7 +30,7 @@ public class FindLoopTest {
 
     @Test
     public void whenFind3() {
-        int[] input = new int[] {5, 2, 10, 2, 4};
+        int[] input = new int[]{5, 2, 10, 2, 4};
         int value = 2;
         int start = 2;
         int finish = 4;
@@ -39,12 +41,28 @@ public class FindLoopTest {
 
     @Test
     public void whenNotFind5() {
-        int[] input = new int[] {5, 1, 10, 2, 4, 7};
+        int[] input = new int[]{5, 1, 10, 2, 4, 7};
         int value = 1;
         int start = 2;
         int finish = 5;
         int result = FindLoop.indexOf(input, value, start, finish);
         int expect = -1;
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSort5() {
+        int[] input = new int[]{3, 4, 1, 2, 5};
+        int result[] = sort(input);
+        int[] expect = new int[]{1, 2, 3, 4, 5};
+        assertThat(result, is(expect));
+    }
+
+    @Test
+    public void whenSort3() {
+        int[] input = new int[]{30, 40, 9};
+        int result[] = sort(input);
+        int[] expect = new int[]{9, 30, 40};
         assertThat(result, is(expect));
     }
 }
