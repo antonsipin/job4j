cd43ad4ca24ad0f2547b427f58898f6a3e2d5948
antonsipin <anton.sipin@gmail.com>
2019-11-02 20:07:23 +0300

    Слово начинается с ...[#189053]

--- chapter_001/src/test/java/ru/job4j/array/anton/ArrayCharTest.java
+++ chapter_001/src/test/java/ru/job4j/array/anton/ArrayCharTest.java
@@ -16,7 +16,7 @@ public class ArrayCharTest {
     @Test
     public void whenNotStartWithPrefixThenFalse() {
         char[] word = {'H', 'e', 'l', 'l', 'o'};
-        char[] pref = {'H', 'i'};
+        char[] pref = {'i', 'e'};
         boolean result =ArrayChar.startsWith(word, pref);
         assertThat(result, is(false));
     }
