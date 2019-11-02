83eb16309ff14fbb22245708b1cf02f367ddecf0
antonsipin <anton.sipin@gmail.com>
2019-11-02 13:55:06 +0300

    Массив заполнен true или false[#189054]

---
+++ chapter_001/src/main/java/ru/job4j/array/anton/Check.java
@@ -0,0 +1,12 @@
+package ru.job4j.array.anton;
+
+public class Check {
+    public boolean mono(boolean[] data) {
+        boolean result = true;
+        for (int index = 0; index < data.length; index++) {
+            if (data[index] == data[0]) {
+            } else result = false;
+        }
+        return result;
+    }
+}
