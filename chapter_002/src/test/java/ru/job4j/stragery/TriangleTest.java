e54de393e2b8ae9e1215a52fdd10e95d3163ac40
antonsipin <anton.sipin@gmail.com>
2020-01-11 15:01:22 +0300

    7. Шаблон проектирования - Стратегия.[#189152]

---
+++ chapter_002/src/test/java/ru/job4j/stragery/TriangleTest.java
@@ -0,0 +1,29 @@
+package ru.job4j.stragery;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author Anton Sipin (anton.sipin@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
+public class TriangleTest {
+    @Test
+    public void whenDrawTriangle() {
+        Triangle triangle = new Triangle();
+        assertThat(
+                triangle.draw(),
+                is(
+                        new StringBuilder()
+                                .append("   +")
+                                .append(" +   +")
+                                .append("+     +")
+                                .append("++++")
+                                .toString()
+                )
+        );
+    }
+}
