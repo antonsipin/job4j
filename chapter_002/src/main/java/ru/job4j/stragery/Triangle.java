52869c90d41421a488e4cf4ebee3aad3d9192b79
antonsipin <anton.sipin@gmail.com>
2020-01-15 23:37:52 +0300

    9.1 Рефакторинг теста @Before @After[#189147]

--- chapter_002/src/main/java/ru/job4j/stragery/Triangle.java
+++ chapter_002/src/main/java/ru/job4j/stragery/Triangle.java
@@ -1,13 +1,17 @@
 package ru.job4j.stragery;
 
+import java.util.StringJoiner;
+
 public class Triangle implements Shape {
     @Override
     public String draw() {
-        StringBuilder pic = new StringBuilder();
-        pic.append("   +" + '\r' + '\n');
-        pic.append(" +   +" + '\r' + '\n');
-        pic.append("+     +" + '\r' + '\n');
-        pic.append(" +++++" + '\r' + '\n');
+        StringJoiner pic = new StringJoiner(
+        System.lineSeparator(), "",
+        System.lineSeparator())
+        .add("   +")
+        .add(" +   +")
+        .add("+     +")
+        .add(" +++++");
         return pic.toString();
     }
 }
