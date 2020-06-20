6c8d772d634217e6364e193f15e8c3a727818117
antonsipin <anton.sipin@gmail.com>
2020-06-20 15:05:57 +0300

    1. Телефонный справочник на базе ArrayList [#189122]

---
+++ chapter_003/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -0,0 +1,49 @@
+package ru.job4j.search;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class PhoneDictionaryTest {
+    @Test
+    public void whenFindByName() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("P");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindBySurname() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("A");
+        assertThat(persons.get(0).getSurname(), is("Arsentev"));
+    }
+
+    @Test
+    public void whenFindByPhone() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("48");
+        assertThat(persons.get(0).getPhone(), is("534872"));
+    }
+
+    @Test
+    public void whenFindByAddress() {
+        PhoneDictionary phones = new PhoneDictionary();
+        phones.add(
+                new Person("Petr", "Arsentev", "534872", "Bryansk")
+        );
+        ArrayList<Person> persons = phones.find("Bry");
+        assertThat(persons.get(0).getAddress(), is("Bryansk"));
+    }
+}
